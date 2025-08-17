e3c120d4e13e13e54b644a9e53d3235cbfceee2d
Michael <mvasilyev04@gmail.com>
2025-08-17 20:58:33 +0300

    Add new struct for DB and func for struct

--- src/config/config.go
+++ src/config/config.go
@@ -11,11 +11,34 @@ type Config struct {
 	Port string
 }
 
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	DBName   string
+	SSLMode  string
+}
+
 func Create() Config {
 	if err := godotenv.Load("../../.env"); err != nil {
 		log.Fatal("Error: .env file not found")
 	}
 	return Config{
 		Port: os.Getenv("PORT"),
+	}
+}
+
+func DBConfigCreate() DBConfig {
+	if err := godotenv.Load("../../.env"); err != nil {
+		log.Fatal("Error: .env file not found")
+	}
+	return DBConfig{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   os.Getenv("DBName"),
+		SSLMode:  os.Getenv("SSLMode"),
 	}
 }
