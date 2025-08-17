c7d5bfa06f1539462531955a6e398c95c8403c39
Michael <mvasilyev04@gmail.com>
2025-08-17 20:59:10 +0300

    Add struct and interface for database conn

--- src/storage/storage.go
+++ src/storage/storage.go
@@ -1,1 +1,27 @@
 package storage
+
+import (
+	"context"
+	"fmt"
+	"shortener/src/config"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type DataBase struct {
+	db *pgxpool.Pool
+}
+
+type Repository interface {
+	Save(original_url string, db *DataBase) (int, error)
+	Get(id int, db *DataBase) (string, error)
+}
+
+func DbConnect(cfg config.DBConfig) (*DataBase, error) {
+	connString := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DBName, cfg.SSLMode)
+	conn, err := pgxpool.New(context.Background(), connString)
+	if err != nil {
+		return nil, err
+	}
+	return &DataBase{db: conn}, nil
+}
