821dd4a26cd4d7687eecc856d2aa27401b2038fc
Michael <mvasilyev04@gmail.com>
2025-09-03 21:48:40 +0300

    Добавлены логи

--- src/storage/storage.go
+++ src/storage/storage.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"context"
 	"fmt"
+	"log"
 	"shortener/src/config"
 
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -22,28 +23,28 @@ type Closer interface {
 }
 
 func DbConnect(cfg config.DBConfig) (*DataBase, error) {
-	connString := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DBName, cfg.SSLMode)
+	connString := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?search_path=public&sslmode=%s", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DBName, cfg.SSLMode)
 	conn, err := pgxpool.New(context.Background(), connString)
 	if err != nil {
+		log.Printf("%v", err)
 		return nil, err
 	}
+	log.Println("Succesful connection to DataBase")
 	return &DataBase{db: conn}, nil
 }
 
 func (d *DataBase) Save(original_url string) (int, error) {
-	_, err := d.db.Exec(context.Background(), "INSERT INTO urls (original_url) VALUES ($1)", original_url)
+	var id int
+	err := d.db.QueryRow(context.Background(), "INSERT INTO public.urls (original_url) VALUES ($1) RETURNING id", original_url).Scan(&id)
 	if err != nil {
 		return 0, err
 	}
-	var id int
-	row := d.db.QueryRow(context.Background(), "SELECT id FROM urls WHERE original_urls=$1", original_url)
-	row.Scan(&id)
 	return id, nil
 }
 
 func (d *DataBase) Get(id int) string {
 	var url string
-	row := d.db.QueryRow(context.Background(), "SELECT original_url FROM urls WHERE id=$1", id)
+	row := d.db.QueryRow(context.Background(), "SELECT original_url FROM public.urls WHERE id=$1", id)
 	row.Scan(url)
 	return url
 }
