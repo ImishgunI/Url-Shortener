3801053dee5c8e667e20841d3ff1d05d0ed84df4
Michael <mvasilyev04@gmail.com>
2025-09-03 21:48:04 +0300

    Добавил post endpoing в роутер gin

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,15 +2,25 @@ package main
 
 import (
 	"shortener/src/config"
+	"shortener/src/storage"
+	"shortener/src/url"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	cfg := config.Create()
+	db_cfg := config.DBConfigCreate()
+	db, err := storage.DbConnect(db_cfg)
+	if err != nil {
+		panic(err)
+	}
+	h := url.New(db)
 	r := gin.Default()
-	err := r.Run(cfg.Port)
+	r.POST("/shorten", h.PostShortUrl)
+	err = r.Run(cfg.Port)
 	if err != nil {
 		panic("Cannot run server on port " + cfg.Port)
 	}
+	db.Close()
 }
